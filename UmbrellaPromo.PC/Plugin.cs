8cce60e27b8014b1b4ccf543f758266278d7137a
Riley <90689870+speecil@users.noreply.github.com>
2024-04-01 00:25:39 +1100

    Init

---
+++ UmbrellaPromo.PC/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using SiraUtil.Zenject;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+namespace UmbrellaPromo.PC
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    [NoEnableDisable]
+    public class Plugin
+    {
+        [Init]
+        public void Init(IPALogger logger, IPA.Config.Config conf, Zenjector zenjector)
+        {
+            zenjector.UseLogger(logger);
+            zenjector.UseHttpService();
+        }
+    }
+}
